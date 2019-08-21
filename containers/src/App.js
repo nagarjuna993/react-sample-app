07a1f8ec07f3d52cacc7958bb3f686cb53513ab6
Nagarjuna <nagarjuna993@hotmail.com>
2019-08-22 00:39:55 +0530

    Updated Containers app to latest dependencies

--- containers/src/App.js
+++ containers/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,22 +8,22 @@ import Rest from './containers/Rest';
 import ForLoop from './containers/ForLoop';
 import Condition from './containers/Condition';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         <Basic></Basic>
         {/* <Counter></Counter>
         <Rest></Rest>
         <ForLoop></ForLoop>
         <Condition></Condition> */}
-      </div>
-    );
-  }
+      </header>
+    </div>
+  );
 }
 
 export default App;
